7b394392e24def1d5517aab9b33ec53839666b8c
Brenda Andreia <brendaandreia@gmail.com>
2021-08-30 19:24:15 -0300

    testimonial start and end

--- js/main.js
+++ js/main.js
@@ -20,3 +20,55 @@
         }
     })
 })();
+
+/*------------- slide depoimento ------------ */
+
+(() =>{
+
+    const sliderContainer = document.querySelector(".testi-slider-container"),
+    slides = sliderContainer.querySelectorAll(".testi-item"),
+    slideWidth = sliderContainer.offsetWidth,
+    prevBtn = document.querySelector(".testi-slider-nav .prev"),
+    nextBtn = document.querySelector(".testi-slider-nav .next"),
+    activeSlide = sliderContainer.querySelector(".testi-item.active");
+    let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+    
+    //setar largura pros slides 
+    slides.forEach((slide) =>{
+        slide.style.width = slideWidth + "px";
+    })
+    
+    //setar largura do sliderContainer
+    sliderContainer.style.width = slideWidth * slides.length + "px";
+
+    nextBtn.addEventListener("click", () =>{
+        if(slideIndex === slides.length-1){
+            slideIndex = 0;
+        }
+        else{
+            slideIndex++;
+        }
+        slider();
+    })
+
+    prevBtn.addEventListener("click", () =>{
+        if(slideIndex === 0){
+            slideIndex = slides.length-1;
+        }
+        else{
+            slideIndex--;
+        }
+        slider();
+    })
+
+    function slider(){
+        //desativa o stlide ativo existente
+        sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+        //novo slide ativo
+        slides[slideIndex].classList.add("active");
+        sliderContainer.style.marginLeft = - (slideWidth *slideIndex) + "px";
+    }
+    slider();
+
+})();
+
